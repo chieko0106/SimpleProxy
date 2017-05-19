df92a496be6d6a788000f23e15918e308582e822
jc3213 <jc3213@users.noreply.github.com>
2017-05-20 07:35:06 +0800

    fixed a bug in latest thunder bird

--- bootstrap.js
+++ bootstrap.js
@@ -1,29 +1,47 @@
 "use strict";
 
-var Cu = Components.utils;
+var Cc = Components.classes, Ci = Components.interfaces, Cu = Components.utils;
+var app = Components.classes["@mozilla.org/xre/app-info;1"].getService(Components.interfaces.nsIXULAppInfo).ID
 var loader;
 
 function startup(data, reason) {
   Cu.import("chrome://simpleproxy/content/events.js");
   Cu.import("chrome://simpleproxy/content/proxy.js");
   Cu.import("chrome://simpleproxy/content/config.js");
-  Cu.import("chrome://simpleproxy/content/toolbar.js");
 
-  loader = {
-    on: function () {
-      Events.on();
-      Proxy.on();
-      Configuration.on();
-      Toolbar.on();
-    },
-    off: function () {
-      Events.off();
-      Proxy.off();
-      Configuration.off();
-      Toolbar.off();
-    }
-  };
+console.log(app)
+  if (app == "{ec8030f7-c20a-464f-9b0e-13a3a9e97384}") {
+    Cu.import("chrome://simpleproxy/content/toolbar.js");
 
+    loader = {
+      on: function () {
+        Events.on();
+        Proxy.on();
+        Configuration.on();
+        Toolbar.on();
+      },
+      off: function () {
+        Events.off();
+        Proxy.off();
+        Configuration.off();
+        Toolbar.off();
+      }
+    };
+  }
+  else {
+    loader = {
+      on: function () {
+        Events.on();
+        Proxy.on();
+        Configuration.on();
+      },
+      off: function () {
+        Events.off();
+        Proxy.off();
+        Configuration.off();
+      }
+    };
+  }
   loader.on();
 }
 
@@ -35,9 +53,11 @@ function shutdown(data, reason) {
   Cu.unload("chrome://simpleproxy/content/events.js");
   Cu.unload("chrome://simpleproxy/content/proxy.js");
   Cu.unload("chrome://simpleproxy/content/worker.js");
-  Cu.unload("chrome://simpleproxy/content/toolbar.js");
   Cu.unload("chrome://simpleproxy/content/worker/pac.js");
   Cu.unload("chrome://simpleproxy/content/worker/editor.js");
+  if (app == "{ec8030f7-c20a-464f-9b0e-13a3a9e97384}") {
+    Cu.unload("chrome://simpleproxy/content/toolbar.js");
+  }
   Cu.unload("resource://simpleproxy/file-io.js");
   Cu.unload("resource://simpleproxy/makepattern.js");
   Cu.unload("resource://simpleproxy/pref-tuils.js");
