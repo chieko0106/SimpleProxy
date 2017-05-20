7862f7f99ee693950fcdd95fce2a44adc2c16cf0
jc3213 <jc3213@users.noreply.github.com>
2017-05-21 07:45:22 +0800

    oops

--- content/worker/pac.js
+++ content/worker/pac.js
@@ -33,7 +33,7 @@ var PAC = {
   match: function () {
     var num = Preferences.getValue( { name: "number", type: "integer" } );
     var array = [];
-	if (num == 1) {
+    if (num == 1) {
       if (Storage[i].match == undefined || Storage[i].match.length == 0 || Storage[i].host == undefined) return "";
       var match = Storage[0].match, host = Storage[0].host;
       var _match = "match: [" + match.join() + "],";
