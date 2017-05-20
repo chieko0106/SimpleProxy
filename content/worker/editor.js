38c0c55f1968be8484adda2865c5a2007bf4f91a
jc3213 <jc3213@users.noreply.github.com>
2017-05-20 08:10:46 +0800

    can't get "navigator:browser" in thunder bird

--- content/worker/editor.js
+++ content/worker/editor.js
@@ -3,14 +3,20 @@
 var EXPORTED_SYMBOLS = ["Editor"];
 
 var Cc = Components.classes, Ci = Components.interfaces, Cu = Components.utils;
-var wm = Cc["@mozilla.org/appshell/window-mediator;1"].getService(Ci.nsIWindowMediator);
+var window = Cc["@mozilla.org/appshell/window-mediator;1"].getService(Ci.nsIWindowMediator).getMostRecentWindow("");
+var app = Components.classes["@mozilla.org/xre/app-info;1"].getService(Components.interfaces.nsIXULAppInfo).ID
 Cu.import("chrome://simpleproxy/content/core.js");
 
 var Editor = {
   open: function (storage) {
     if (!storage.file) return;
-    var window = wm.getMostRecentWindow("navigator:browser");
-    var ScratchpadManager = window.Scratchpad.ScratchpadManager;
+
+    if (app == "{ec8030f7-c20a-464f-9b0e-13a3a9e97384}") {
+      var ScratchpadManager = window.Scratchpad.ScratchpadManager;
+    }
+    else {
+      var ScratchpadManager = window.ScratchpadManager;
+    }
 
     ScratchpadManager.openScratchpad({
       "filename": storage.file,
